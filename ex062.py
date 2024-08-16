42e8620ed6b8343f0621514de96e68f43f9d2415
eder-castro <132703429+eder-castro@users.noreply.github.com>
2024-08-15 23:52:24 -0300

    Ex 062 atualizado

--- ex062.py
+++ ex062.py
@@ -1,12 +1,19 @@
 print ("=×" * 15)
-print ("{: ^32}".format(" 10 TERMOS DE UMA P.A. " ))
+print ("{: ^32}".format(" TERMOS DE UMA P.A. " ))
 print ("=×" * 15)
 termo = int(input("Digite o primeiro termo: "))
 fator = int(input("Digite o fator da progressão: "))
-print (f"{termo} ", end = '')
+print (f"{termo} => ", end = '')
 num = termo
+qtd = 10
 cont = 1
-while (cont < 10):
-    num += fator
-    cont += 1
-    print (f" => {num}", end = '')
+soma = 10
+while (qtd != 0) :
+    while (cont < qtd):
+        num += fator
+        print (f"{num} => ", end = '')
+        cont += 1
+    print("PAUSA")
+    qtd = int(input("Quantos termos mais? "))
+    soma += qtd
+print(f"Progressao finalizada com {soma} termos!")
